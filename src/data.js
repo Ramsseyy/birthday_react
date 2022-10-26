e15df448474df3c581893a3933b9709600c2d790
Ramsseyy <94022198+Ramsseyy@users.noreply.github.com>
2022-10-26 15:16:57 -0700

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export default [
+    {
+      id: 1,
+      name: 'Landon Volllgreafv',
+      age: 17,
+
+    },
+    {
+      id: 2,
+      name: 'Brian Mercado',
+      age: 18,
+
+    },
+    {
+      id: 3,
+      name: 'Sam Mun',
+      age: 16,
+
+    },
+    {
+      id: 4,
+      name: 'Juan Vober',
+      age: 17,
+
+    },
+  ];
